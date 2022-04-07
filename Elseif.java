d56ee1dcb1b83d57dd06bb9c5f24f280876fa94d
sakshi665 <65933652+sakshi665@users.noreply.github.com>
2022-04-07 15:42:10 +0530

    Add files via upload

---
+++ Elseif.java
@@ -0,0 +1,26 @@
+package April_o6_2022;
+
+public class Elseif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=10;
+		int n2=20;
+		
+		int n3=47;
+		if(n1>n2 && n1>n3)
+		{
+			System.out.print("n1 is greater");
+		}
+		else if(n2>n1 && n2>n3)
+		{
+			System.out.print("n2 is greater");
+		}
+		else
+		{
+			System.out.print("n3 is greater");
+		}
+
+	}
+
+}
