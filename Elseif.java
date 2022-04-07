a629b87c06aae32ab80e8c883691a1e64eebb0ce
sakshi665 <65933652+sakshi665@users.noreply.github.com>
2022-04-07 15:42:46 +0530

    Update Elseif.java

--- Elseif.java
+++ Elseif.java
@@ -6,9 +6,8 @@ public class Elseif {
 		// TODO Auto-generated method stub
 		int n1=10;
 		int n2=20;
-		
 		int n3=47;
-		if(n1>n2 && n1>n3)
+		if(n1>=n2 && n1>=n3)
 		{
 			System.out.print("n1 is greater");
 		}
