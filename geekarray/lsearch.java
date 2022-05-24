6ede58883c383029fc652562a1900eee98ea4151
sakshi665 <65933652+sakshi665@users.noreply.github.com>
2022-05-24 13:22:43 +0530

    Add files via upload

---
+++ geekarray/lsearch.java
@@ -0,0 +1,30 @@
+package geekarray;
+import java.util.*;
+public class lsearch {
+
+	static int search(int[] arr,int ele)
+	{
+	    for(int i=0;i<arr.length;i++)
+	    {
+	        if(arr[i]==ele)
+	        {
+	            return i;
+	        }
+	    }
+	    return -1;
+	}
+	 public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int[] arr=new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        arr[i]=sc.nextInt();
+	    }
+	    int c=search(arr,n);
+	    System.out.println(c); 
+	}
+	}
+
+
+
