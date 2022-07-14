caa632d4b3534fcdeef31ae119224e4b97a73fd9
sakshi665 <65933652+sakshi665@users.noreply.github.com>
2022-07-14 13:48:37 +0530

    Add files via upload

---
+++ javapro/makegui.java
@@ -0,0 +1,60 @@
+package javapro;
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+public class makegui {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		       JFrame frame = new JFrame("sakshi gaye");
+//		       //sframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		       frame.setSize(500,500);
+//		       JButton button = new JButton("hello click me");
+//		       frame.getContentPane().add(button); // Adds Button to content pane of frame
+//		       frame.setVisible(true);
+		
+		JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        JMenuItem m33 = new JMenuItem("close");
+        m1.add(m11);
+        m1.add(m22);
+        m1.add(m33);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(10); // accepts upto 10 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        
+        // Text Area at the Center
+        JTextArea ta = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+    }
+}
+		  
+		
+
+	
+
+
